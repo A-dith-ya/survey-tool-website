297f82b50af7c33d6c9230bc7befbd92fd9d90fd
A-dith-ya <awedithya@gmail.com>
2023-02-19 21:10:30 -0800

    Add Dashboard page

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
-import "./Login.css";
-import { Link } from "react-router-dom";
+import React from "react";
+import styles from "./Dashboard.module.css";
+import SurveyList from "../../components/SurveyList/SurveyList";
+import { surveys } from "../../dummy/SurveyListDummy";
+import UserHeader from "../../components/UserHeader/UserHearder";
+import { useNavigate } from "react-router-dom";
 
 const DashboardPage = () => {
+  const navigation = useNavigate();
+
   return (
-    <div>
-      <h1></h1>
+    <div className={styles.container}>
+      <UserHeader />
+      <h1>Survey List</h1>
+      <button
+        className={styles.createButton}
+        onClick={() => navigation("/survey")}
+      >
+        Create Survey
+      </button>
+      <SurveyList surveys={surveys} />
     </div>
   );
 };
