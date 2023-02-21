29daeff5e4fa2a91f0fae676e766e4990df0c289
A-dith-ya <awedithya@gmail.com>
2023-02-20 23:52:32 -0800

    Add placeholder data

---
+++ frontend/src/dummy/SurveyListDummy.js
@@ -0,0 +1,27 @@
+export const surveys = [
+  {
+    id: 1,
+    title: "Customer Satisfaction Survey",
+    dateCreated: "2022/04/23",
+  },
+  {
+    id: 2,
+    title: "Product Feedback Survey",
+    dateCreated: "2022/06/10",
+  },
+  {
+    id: 3,
+    title: "Website Usability Survey",
+    dateCreated: "2022/08/01",
+  },
+  {
+    id: 4,
+    title: "Employee Engagement Survey",
+    dateCreated: "2022/09/14",
+  },
+  {
+    id: 5,
+    title: "Market Research Survey",
+    dateCreated: "2022/10/29",
+  },
+];
